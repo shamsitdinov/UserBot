dd36deaf6aa7847f0cc8ec7c45cd7b7d6241a774
Nodirbek <shamsiddinovnodirbek469@gmail.com>
2025-04-28 08:15:43 +0500

    k

--- words.js
+++ words.js
@@ -2,4 +2,4 @@ import mongoose from "mongoose";
 const WordSchema = new mongoose.Schema({
     text: { type: String, required: true },
 })
-export const WordModel = mongoose.model("Word", WordSchema)
+export const WordModel = mongoose.model("Word", WordSchema)  
